a72d16d86793ed5fa1eaf6eaf4c2cc736627c3dc
kongruksiamza <kongruksiamza123@gmail.com>
2020-02-05 00:41:47 +0700

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    private Transform player;
+    public Vector3 offset;
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
