188cfdd3fad175517a03419aa9851395d9ae7cf9
kongruksiamza <kongruksiamza123@gmail.com>
2020-02-05 01:38:16 +0700

    Add files via upload

---
+++ FixedButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FixedButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+}
